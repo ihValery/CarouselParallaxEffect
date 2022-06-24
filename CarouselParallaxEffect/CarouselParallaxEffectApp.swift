bfddd449308c6946cad690975f11a116676dbdb2
ihValery <ihValery@gmail.com>
2022-06-24 05:27:39 +0300

    Initial Commit

---
+++ CarouselParallaxEffect/CarouselParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselParallaxEffectApp.swift
+//  CarouselParallaxEffect
+//
+//  Created by Валерий Игнатьев on 24.06.22.
+//
+
+import SwiftUI
+
+@main
+struct CarouselParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
